e1d7aa00c84afc361037e41e969fb5d9dd38e255
Olha Pushkar <olha.pushkar11@gmail.com>
2023-02-14 19:12:10 +0200

    change config, resolve dependency conflict

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { ButtonProps } from "./Button.props";
-import styles from './Button.module.css';
+import styles from './Button.module.css'
 import cn from 'classnames';
 
-export const Button = ({ appearance, children, className, ...props }: ButtonProps): JSX.Element => {
+export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
     return (
         <button className={cn(styles.button, className, {
             [styles.primary]: appearance == 'primary',
@@ -12,6 +12,10 @@ export const Button = ({ appearance, children, className, ...props }: ButtonProp
         {...props}
         >
             {children}
+            {arrow != 'none' && <span className={cn(styles.arrow, {
+                [styles.down]: arrow == 'down'
+            })}>
+                d</span>}
         </button>
     )
 }
